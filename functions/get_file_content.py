bd1398393b1da5425ad2e82f1360358db8670c76
eric-lund <ericslund@gmail.com>
2025-11-28 08:50:19 -0700

    get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,32 @@
+import os
+from functions.config import MAX_CHAR
+
+def get_file_content(working_directory, file_path):
+    
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        abs_target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+        # Debugging
+        # print(abs_working_dir)
+        # print(abs_target_file)
+        
+        # check if outside working directory
+        if os.path.commonpath([abs_working_dir, abs_target_file]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory\n'
+        
+        # check if the file is valid
+        if os.path.isfile(abs_target_file) == False:
+            return f'Error: File not found or is not a regular file: "{file_path}"\n'
+
+        with open(abs_target_file, "r") as f:
+            # Truncate the string if it's too long
+            file_content = f.read(MAX_CHAR)
+        
+            if len(f.read()) > MAX_CHAR:
+                file_content = file_content + f'\n\n[...File "{file_path}" truncated at 10000 characters]'
+            
+        return file_content
+            
+    except Exception as e:
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,18 +1,34 @@
 import unittest
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 class TestFunctions(unittest.TestCase):
-    def test_current_directory(self):
-        print(f'Results for current directory:\n{get_files_info("calculator",".")}\n')
+    # def test_current_directory(self):
+    #     print(f'Results for current directory:\n{get_files_info("calculator",".")}\n')
         
-    def test_pkg_directory(self):
-        print(f'Results for \'pkg\' directory:\n{get_files_info("calculator","pkg")}\n')
+    # def test_pkg_directory(self):
+    #     print(f'Results for \'pkg\' directory:\n{get_files_info("calculator","pkg")}\n')
 
-    def test_bin_filepath(self):
-        print(f'Results for \'/bin\' directory:\n{get_files_info("calculator", "/bin")}\n')
+    # def test_bin_filepath(self):
+    #     print(f'Results for \'/bin\' directory:\n{get_files_info("calculator", "/bin")}\n')
 
-    def test_relative_parent_filepath(self):
-        print(f'Results for \'../\' directory:\n {get_files_info("calculator", "../")}\n')
+    # def test_relative_parent_filepath(self):
+    #     print(f'Results for \'../\' directory:\n {get_files_info("calculator", "../")}\n')
+
+    def test_main(self):
+        print(f'{get_file_content("calculator", "main.py")}')
+
+    def test_calculator(self):
+         print(f'{get_file_content("calculator", "pkg/calculator.py")}')
+
+    def test_bin_cat(self):
+        print(f'{get_file_content("calculator", "/bin/cat")}')
+
+    def test_pkg_fake_file(self):
+        print(f'{get_file_content("calculator", "pkg/does_not_exist.py")}')
+   
+    # def test_lorem(self):
+        # print(f'{get_file_content("calculator", "lorem.txt")}')
 
 if __name__ == "__main__":
     unittest.main()
