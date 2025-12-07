41e3805ee3963121590ceee8340b767d9deea2a3
eric-lund <ericslund@gmail.com>
2025-12-07 15:14:43 -0700

    change from if/else to dictionary of functions

--- functions/call_function.py
+++ functions/call_function.py
@@ -11,12 +11,28 @@ def call_function(function_call_part, verbose=False):
         print(f" - Calling function: {function_call_part.name}")
 
     working_directory = {"working_directory": "./calculator"}
-    arguments = (function_call_part.args)
+    function_name = function_call_part.name
+    arguments = function_call_part.args
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+    target_function = function_map[function_name]
+    result = target_function(**working_directory, **arguments)
 
-    if function_call_part.name == 'get_files_info':
-        results = get_files_info(**working_directory)
-    elif function_call_part.name == "get_file_content":
-        results = get_file_content(**working_directory, **arguments) 
+
+
+
+    # if function_call_part.name == 'get_files_info':
+    #     results = get_files_info(**working_directory)
+    # elif function_call_part.name == "get_file_content":
+    #     results = get_file_content(**working_directory, **arguments)
+    # elif function_call_part.name == 'write_file':
+    #     results = write_file(**working_directory, **arguments)
+    # elif function_call_part.name == 'run_python_file':
+    #     results = run_python_file(**working_directory, **arguments)
 
     print(results)
     
